d8c87a98fbc0784f2c238fa4e9c3def6a33df6df
unknown <benach@gmail.com>
2023-04-01 20:40:55 +0200

    main.py with some code

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-some_code = 1
-print("Hello World")
+import numpy as np
+
+matrix = np.array([[1, 2], [3, 4], [5, 6]])
+
+arr = matrix.reshape(-1)
+
+mean = np.mean(arr)
+
+std = np.std(arr)
+
+new_matrix = arr.reshape(matrix.shape[0], matrix.shape[1])
+
+mult_matrix = np.multiply(matrix, new_matrix)
+
+sum_mult_matrix = np.sum(mult_matrix)
+
+print(f"Original matrix:\n{matrix}")
+print(f"Reshaped 1D array:\n{arr}")
+print(f"Mean of 1D array:\n{mean}")
+print(f"Standard deviation of 1D array:\n{std}")
+print(f"Reshaped 2D matrix:\n{new_matrix}")
+print(f"Element-wise multiplied matrix:\n{mult_matrix}")
+print(f"Sum of all elements in element-wise multiplied matrix:\n{sum_mult_matrix}")
